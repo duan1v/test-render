81484cb482c7583dc4bf5dc8fefa2417129d22a9
duan1v <yiwei.duan@outlook.com>
2023-10-08 19:34:42 +0800

    1

--- public/index.php
+++ public/index.php
@@ -4,7 +4,8 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-
+phpinfo();
+die();
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
