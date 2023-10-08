e30ffeab6c6c571daa3f088c0a651a8ff224e90d
duan1v <yiwei.duan@outlook.com>
2023-10-08 22:22:11 +0800

    1

--- config/livewire.php
+++ config/livewire.php
@@ -52,6 +52,8 @@ return [
 
     'lazy_placeholder' => null,
 
+    'asset_url' => env('APP_URL', 'http://localhost'),
+
     /*
     |---------------------------------------------------------------------------
     | Temporary File Uploads
@@ -64,14 +66,14 @@ return [
     */
 
     'temporary_file_upload' => [
-        'disk' => null,        // Example: 'local', 's3'              | Default: 'default'
-        'rules' => null,       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
-        'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
-        'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
-        'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
-            'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
-            'mov', 'avi', 'wmv', 'mp3', 'm4a',
-            'jpg', 'jpeg', 'mpga', 'webp', 'wma',
+        'disk'            => null,        // Example: 'local', 's3'              | Default: 'default'
+        'rules'           => null,       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
+        'directory'       => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
+        'middleware'      => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
+        'preview_mimes'   => [   // Supported file types for temporary pre-signed file URLs...
+                                 'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
+                                 'mov', 'avi', 'wmv', 'mp3', 'm4a',
+                                 'jpg', 'jpeg', 'mpga', 'webp', 'wma',
         ],
         'max_upload_time' => 5, // Max duration (in minutes) before an upload is invalidated...
     ],
